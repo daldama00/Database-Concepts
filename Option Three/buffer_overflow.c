abe60d2095b9ff963606674f03f3bbf12c23731a
daldama00 <157993636+daldama00@users.noreply.github.com>
2025-04-29 16:35:39 -0500

    Update buffer_overflow.c code

--- Option Three/buffer_overflow.c
+++ Option Three/buffer_overflow.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void vulnerable(char *input) {
+    char buffer[10];
+    strcpy(buffer, input);
+    printf("Buffer content: %s\n", buffer);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) return 1;
+    vulnerable(argv[1]);
+    return 0;
+}
 
