1a5eeb0e22c8ae81f3a1abc8398f50973ebcc19d
daldama00 <157993636+daldama00@users.noreply.github.com>
2025-04-29 16:37:02 -0500

    Update SQL_injection_fixed.py code

--- Option Three/SQL_injection_fixed.py
+++ Option Three/SQL_injection_fixed.py
@@ -1,1 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("CREATE TABLE users (username TEXT, password TEXT)")
+c.execute("INSERT INTO users VALUES ('admin', 'pass')")
+
+username = input("Username: ")
+password = input("Password: ")
+
+c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+print("Login success!" if c.fetchone() else "Login failed.")
 
