fea41a2c080ff7e48ee751dc71cdf16ad02cae48
daldama00 <157993636+daldama00@users.noreply.github.com>
2025-04-29 16:37:22 -0500

    Update Race_condition.c code

--- Option Three/Race_condition.c
+++ Option Three/Race_condition.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main() {
+    char *filename = "temp.txt";
+    struct stat st;
+
+    if (stat(filename, &st) == 0) {
+        printf("File exists, opening...\n");
+        int fd = open(filename, O_RDONLY);
+        char buffer[100];
+        read(fd, buffer, sizeof(buffer) - 1);
+        buffer[99] = '\0';
+        printf("Contents: %s\n", buffer);
+        close(fd);
+    } else {
+        printf("File not found.\n");
+    }
+
+    return 0;
+}
 
